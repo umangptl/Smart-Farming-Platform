39b22f7f77aaea364e43bc457c61344eafd673cf
gemj98 <gemj98@gmail.com>
2025-04-28 12:40:57 -0700

    add direction selector for ow_line_counter

--- backend/app/inference/model_factory.py
+++ backend/app/inference/model_factory.py
@@ -7,7 +7,19 @@ def build_model_from_config(config: dict):
         return get_cow_line_counter(
             normalized_line_start=config["line_start"],
             normalized_line_end=config["line_end"],
-            triggering_anchors=config.get("triggering_anchors")
+            triggering_anchors=directionToAnchor.get(config.get("triggering_anchors"))
         )
     
     raise ValueError(f"Unsupported model: {model_name}")
+    
+directionToAnchor =  {
+    "UP_LEFT": ["TOP_LEFT"],
+    "UP": ["TOP_LEFT", "TOP_RIGHT"],
+    "UP_RIGHT": ["TOP_RIGHT"],
+    "LEFT": ["TOP_LEFT", "BOTTOM_LEFT"],
+    "CENTER": [],
+    "RIGHT": ["TOP_RIGHT", "BOTTOM_RIGHT"],
+    "DOWN_LEFT": ["BOTTOM_LEFT"],
+    "DOWN": ["BOTTOM_LEFT", "BOTTOM_RIGHT"],
+    "DOWN_RIGHT": ["BOTTOM_RIGHT"]
+}
