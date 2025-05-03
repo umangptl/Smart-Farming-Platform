2dece08e854a2cf58d87ebd7b40e6655c2437d9b
Abhilash <tayadeabhilash@gmail.com>
2025-05-03 14:18:20 -0700

    Add dep

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -28,7 +28,7 @@ def create_app():
     # Initialize the Flask app
 
     app = Flask(__name__)
-    app.config["JWT_SECRET_KEY"] = Config.JWT_SECRET_KEY
+    app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY")
     configure_db(app)
     bcrypt.init_app(app)
     jwt.init_app(app)
